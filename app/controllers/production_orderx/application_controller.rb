949482644dc43d7394cce31cdefefa809b7a4ab2
JC <jc@email.com>
2016-06-30 11:33:46 -0500

    JC: added 2 helper methods.

--- app/controllers/production_orderx/application_controller.rb
+++ app/controllers/production_orderx/application_controller.rb
@@ -15,6 +15,8 @@ module ProductionOrderx
     before_action :view_in_config?
     before_action :page_params, :only => :index
     
+    helper_method :return_users, :return_misc_definitions
+    
     protected
   
     def max_pagination
